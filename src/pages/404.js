e0d388f70a21de107552b734397e0363f9f094c8
GrzegorzBaczek93 <Grzegorz.baczek.93@gmail.com>
2022-01-18 23:20:25 +0100

    Added temporary 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import '@styles/global.css';
+import Layout from '@components/common/layout';
+
+const PageNotFound = props => (
+    <Layout>
+        <div>
+            404 Page not found
+            Sorry mah boi!
+        </div>
+    </Layout>
+);
+
+export default PageNotFound;
